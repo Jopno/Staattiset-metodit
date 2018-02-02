fad5025c0152bba0a25dfa01ace2cf743b66fc16
Jopno <35934376+Jopno@users.noreply.github.com>
2018-02-02 16:11:50 +0200

    Add files via upload

---
+++ Task2/DayApplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItDay
+{
+    class DayApplication
+    {
+        internal static bool IsDay( string input)
+        {
+            DateTime date;
+            string[] pattern = new string[] { "d.M.yyyy", "d.M.yy" };
+            if (DateTime.TryParseExact(input, pattern, System.Globalization.CultureInfo.CurrentCulture,
+                System.Globalization.DateTimeStyles.None, out date))
+            {
+                Console.WriteLine(date.ToString("d.M.yyyy"));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //zulul
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give a date");
+            string line = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("It is finnish date: {0} ", DayApplication.IsDay(line));
+
+        }
+    }
+}
