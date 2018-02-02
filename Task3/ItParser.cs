b63c44941ba3a299b3e2bbd836f52cef8c790461
Jopno <35934376+Jopno@users.noreply.github.com>
2018-02-02 16:55:24 +0200

    Add files via upload

---
+++ Task3/ItParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItParserApplication
+{
+    class ItParser
+    {
+        public static int Count(string line)
+        {
+            int[] array = line.Split(',').Select(Xline => int.Parse(Xline)).ToArray();
+            return array.Length;
+        }
+        public static double Average(string line)
+        {
+            int[] array = line.Split(',').Select(Xline => int.Parse(Xline)).ToArray();
+            return array.Average();
+        }
+
+        public static int Sum(string line)
+        {
+            int[] array = line.Split(',').Select(Xline => int.Parse(Xline)).ToArray();
+            return array.Sum();
+        }
+
+
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItParserApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give numbers separated by comma. ");
+            string line = Console.ReadLine();
+            TestParser(line);
+        }
+
+        public static void TestParser(string line)
+        {
+            Console.WriteLine("Markline {0} include {1} numbers , sum of numbers is {2}" +
+                " and average is {3}", line, ItParser.Count(line), ItParser.Sum(line), ItParser.Average(line));
+        }
+    }
+}
