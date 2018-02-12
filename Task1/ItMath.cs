2e383bf756a192badb2982c6f3c17a82040a0d0c
Jopno <35934376+Jopno@users.noreply.github.com>
2018-02-12 22:10:10 +0200

    Add files via upload

---
+++ Task1/ItMath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ItMathAppli
+{
+    class ItMath
+    {
+        public static bool IsItNumb(string input)
+        {
+            CultureInfo info = CultureInfo.GetCultureInfo("fi-Fi");
+            float numb = 0;
+            int length = input.Length - 1;
+            if (input[0].ToString() != "," && input[length].ToString() != ",")
+            {
+                bool result = float.TryParse(input, NumberStyles.Any, info, out numb);
+                if (result)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Task1/TestiPeti.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItMathAppli
+
+{
+    class Testipeti
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Unvalid numbers");
+
+            Console.WriteLine(ItMath.IsItNumb("Kissa"));
+            Console.WriteLine(ItMath.IsItNumb("abcABC1"));
+            Console.WriteLine(ItMath.IsItNumb("1234."));
+            Console.WriteLine(ItMath.IsItNumb(",1234"));
+            Console.WriteLine(ItMath.IsItNumb("1234,"));
+            Console.WriteLine(ItMath.IsItNumb("12-34"));
+
+            Console.WriteLine("Valid numbers");
+
+            Console.WriteLine(ItMath.IsItNumb("0"));
+            Console.WriteLine(ItMath.IsItNumb("123"));
+            Console.WriteLine(ItMath.IsItNumb("123,45"));
+            Console.WriteLine(ItMath.IsItNumb("+123"));
+            Console.WriteLine(ItMath.IsItNumb("-123"));
+            Console.WriteLine(ItMath.IsItNumb("+123,45"));
+            Console.WriteLine(ItMath.IsItNumb("-123,45"));
+        }
+    }
+}
